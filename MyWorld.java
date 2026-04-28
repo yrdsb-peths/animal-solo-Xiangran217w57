8fb24e3391a6142648037de8fe5f1b64c84934c0
Xiangran217w57 <aha.marianli@gmail.com>
2026-04-28 11:20:04 -0400

    add apple

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,18 @@
 import greenfoot.*;
-
+/**
+ * The World our hero lives in.
+ * 
+ * @author (your name) 
+ * @version April 2026
+ */
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        
+        Apple apple = new Apple();
+        addObject(apple, 300, 0);
     }
 }
