4b3c197ac06381ce31ece649a98e90d9513b3a55
Xiangran217w57 <aha.marianli@gmail.com>
2026-04-28 12:00:49 -0400

    add increase score

--- MyWorld.java
+++ MyWorld.java
@@ -6,13 +6,33 @@ import greenfoot.*;
  * @version April 2026
  */
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
+    
+    /**
+     * Constructor for objects of class MyWorld.
+     */
     public MyWorld() {
         super(600, 400, 1);
         
+        // Create the elephant object
         Elephant elephant = new Elephant();
-        addObject(elephant, 300, 200);
+        addObject(elephant, 300, 300);
+        
+        // Create a label
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
         
         createApple();
+    }
+    
+    /**
+     * Inscrease score
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
     }
     
     public void createApple()
