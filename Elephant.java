56a1791b4cb171ea3e3190126553667fa4497d57
Xiangran217w57 <aha.marianli@gmail.com>
2026-05-05 11:34:23 -0400

    revise imgae

--- Elephant.java
+++ Elephant.java
@@ -9,14 +9,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("Elephant sound.mp3");
-    GreenfootImage idle = new GreenfootImage("images/elephant_idle/idle0.png");
+    GreenfootImage[] idle = new GreenfootImage[8];
     
     /** 
      * Constructor - The code that gets run one time when object is created
      */
     public Elephant()
     {
-        setImage(idle);
+        for(int i = 0; i < idle.length; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
     }
     public void act()
     {
