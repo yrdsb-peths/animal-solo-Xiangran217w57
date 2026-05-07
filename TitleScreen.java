e0034abc4c6c2180dec4de9a270e7b94b1ed014b
Xiangran217w57 <aha.marianli@gmail.com>
2026-05-07 11:11:00 -0400

    add keydown space

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Hungry Elephant", 100);
+    Label titleLabel = new Label("Hungry Elephant", 60);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -18,5 +18,18 @@ public class TitleScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         addObject(titleLabel, getWidth()/2, getHeight()/2);
+    }
+    
+    /**
+     * The main world act loop
+     */
+    public void act()
+    {
+        //start the game if user presses the space bar
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
     }
 }
