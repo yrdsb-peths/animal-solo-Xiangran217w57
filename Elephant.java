2e4a8dab432853461a42034bcd93839c26e3acaa
Xiangran217w57 <aha.marianli@gmail.com>
2026-05-07 09:16:53 -0400

    revise

--- Elephant.java
+++ Elephant.java
@@ -47,7 +47,7 @@ public class Elephant extends Actor
     int imageIndex = 0;
     public void animateElephant()
     {
-        if(animationTimer.millisElapsed() < 1000)
+        if(animationTimer.millisElapsed() < 100)
         {
             return;
         }
